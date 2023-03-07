09edf7a97b84729af3c9e22ccda59902097fcbf4
Marcosbit93 <marcos93bolonese@gmail.com>
2023-03-07 12:19:35 -0300

    print navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Auth from '../src/components/Auth'
 import { Route, Routes } from 'react-router';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path='/auth' element={<Auth/>}/> 
       </Routes>  
