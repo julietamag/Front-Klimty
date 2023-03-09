bc5b159786b9dd5175f7126e6e40a19672d85a6c
NicholasInchauspe2 <inchauspenicholasar@gmail.com>
2023-03-09 14:56:28 -0300

    newChanges

--- src/components/ProductGrid/index.jsx
+++ src/components/ProductGrid/index.jsx
@@ -1,133 +1,43 @@
-import { Label } from '@mui/icons-material'
-import { Masonry } from '@mui/lab'
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import { Label } from "@mui/icons-material";
+import { Masonry } from "@mui/lab";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { setDetail } from "../../state/detail";
-
+import axios from "axios";
 
 export const ProductGrid = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const [itemData, setItemData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/product")
+      .then(
+        (data) => setItemData(data.data)
+      )
+      .then(res => dispatch(setDetail(itemData[0])));
+  }, []);
 
   return (
-       <Masonry columns={4} spacing={2}>
-        {itemData.map((item, index) => ( 
-             <div key={index}>
-            <Label>{index + 1}</Label>
-            <img
-              onClick={()=>dispatch(setDetail(item))}
-              src={`${item.img}?w=162&auto=format`}
-              srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
-              alt={item.title}
-              loading="lazy"
-              style={{
-                borderBottomLeftRadius: 4,
-                borderBottomRightRadius: 4,
-                display: 'block',
-                width: '100%',
-              }}
-            />
-          </div>
-        ))}
-      </Masonry>
-    
-  )
-}
-
-
-
-
-
-
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1627308595229-7830a5c91f9f',
-    title: 'Snacks',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1529655683826-aba9b3e77383',
-    title: 'Tower',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1627328715728-7bcc1b5db87d',
-    title: 'Tree',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1627000086207-76eabf23aa2e',
-    title: 'Camping Car',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1627328561499-a3584d4ee4f7',
-    title: 'Mountain',
-    price:'100',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    price:'100',
-  },
-  {
-    img: "https://collectionapi.metmuseum.org/api/collection/v1/iiif/436535/796067/main-image",
-    title: 'Rosanna',
-    price:'100',
-  },
-];
+    <Masonry columns={4} spacing={2}>
+      {itemData?.map((item, index) => (
+        <div key={index}>
+          <Label>{index + 1}</Label>
+          <img
+            onClick={() => dispatch(setDetail(item))}
+            src={`${item.photo_url}?w=162&auto=format`}
+            srcSet={`${item.photo_url}?w=162&auto=format&dpr=2 2x`}
+            alt={item.name}
+            loading="lazy"
+            style={{
+              borderBottomLeftRadius: 4,
+              borderBottomRightRadius: 4,
+              display: "block",
+              width: "100%",
+            }}
+          />
+        </div>
+      ))}
+    </Masonry>
+  );
+};
