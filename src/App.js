fa9240fe23dc3bb155ecaf34862ba335e5cefa95
RosannaContasti <rosicontasti@gmail.com>
2023-03-09 14:30:08 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import Auth from '../src/components/Auth'
 import { Route, Routes } from 'react-router';
 import { Navbar } from './components/Navbar';
 import Footer from './components/Footer'
+import ProductCard from './components/Card';
+
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
       <Routes>
         <Route path='/auth' element={<Auth/>}/> 
       </Routes>  
