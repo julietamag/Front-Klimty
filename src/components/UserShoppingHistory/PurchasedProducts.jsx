edbf1acdf21fdd30521bb0547ef8f09235fd3a3d
RosannaContasti <rosicontasti@gmail.com>
2023-03-14 19:49:07 -0300

    Adding User Shopping History

---
+++ src/components/UserShoppingHistory/PurchasedProducts.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import TableFooter from "@mui/material/TableFooter";
+
+const PurchasedProducts = (props) => {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row" align="center">
+            <Typography variant="h4">
+            Purchase #{row.cartId}
+            </Typography>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                CART ID #{row.cartId}
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Product</TableCell>
+                    <TableCell>Price (u)</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                    <TableCell align="right">Total price ($)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.cart.products.map((historyRow, i) => (
+                    <TableRow key={i}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.product.updatedAt}
+                      </TableCell>
+                      <TableCell>{historyRow.product.name}</TableCell>
+                      <TableCell>$ {historyRow.product.price}</TableCell>
+                      <TableCell align="right">{historyRow.quantity}</TableCell>
+                      <TableCell align="right">
+                        $ {Math.round(
+                          historyRow.quantity * historyRow.product.price * 100
+                        ) / 100}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                  <TableFooter variant="footer">
+                    <TableRow hover="true" align="right">
+                      <Typography variant="h5" color="text.secondary">
+                        Total($) {row.cart.products?.reduce((amount, item) => {
+                            return amount + (item.product.price * item.quantity)
+                        }, 0)} 
+                      </Typography>
+                    </TableRow>
+                  </TableFooter>
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+};
+
+export default PurchasedProducts;
---
+++ src/components/UserShoppingHistory/index.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import { Box } from "@mui/material";
+import PurchasedProducts from "../UserShoppingHistory/PurchasedProducts";
+
+const ShoppingHistory = () => {
+  const userId = localStorage.getItem("id");
+  const [purchase, setPurchase] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/api/checkout/${userId}/history`)
+      .then((res) => setPurchase(res.data));
+  }, []);
+
+  return (
+    <Box sx={{ margin: 15 }} >
+      <TableContainer component={Paper} sx={{ boxShadow: 3 }}>
+        <Table aria-label="collapsible table">
+          <TableBody>
+            {purchase.map((row, i) => (
+              <PurchasedProducts key={i} row={row} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default ShoppingHistory;
